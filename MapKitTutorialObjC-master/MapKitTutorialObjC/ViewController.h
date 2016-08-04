993b9e278d7de7fee0172c414b03525b418823ca
mbiswabaran <biswabaranmukherjee@biswabaransmbp2.in.ibm.com>
2016-08-04 18:36:06 +0530

    test

---
+++ MapKitTutorialObjC-master/MapKitTutorialObjC/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  MapKitTutorialObjC
+//
+//  Created by Robert Chen on 1/19/16.
+//  Copyright © 2016 Robert Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import MapKit;
+
+@protocol HandleMapSearch <NSObject>
+- (void)dropPinZoomIn:(MKPlacemark *)placemark;
+@end
+
+@interface ViewController : UIViewController <CLLocationManagerDelegate, HandleMapSearch, MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet UIView *vw_UserSelection;
+
+@end
+
